451859ae4608685f1a9c74a5223920bdb642a0df
Divya Kelaskar <divyakelaskar@gmail.com>
2024-05-06 10:34:12 +0530

    new task overview page

---
+++ Frontend/src/cssfiles/TaskOverview2.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import deletebtn from '../assets/delete.png';
+import editbtn from '../assets/edit-text.png';
+import infobtn from '../assets/information-sign.png';
+import '../cssfiles/TaskOverview2.css';
+import TaskDetailsPopup from '../components/TaskDetailsPopup';
+import EditTask from '../components/EditTask';
+import AssignTask from '../components/AssignTask';
+
+function TaskOverview2() {
+}
+
+export default TaskOverview2;
