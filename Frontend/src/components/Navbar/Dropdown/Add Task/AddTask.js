8fc159388660c18e6c10e02ae829fcef6906fa91
Divya Kelaskar <divyakelaskar@gmail.com>
2024-05-16 15:50:52 +0530

    data fetching done in select project dropdown

--- Frontend/src/components/Navbar/Dropdown/Add Task/AddTask.js
+++ Frontend/src/components/Navbar/Dropdown/Add Task/AddTask.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Button,
   Checkbox,
@@ -15,14 +15,17 @@ import {
   TextField,
 } from '@mui/material';
 import './AddTask.css'
+import axios from 'axios';
 
 const AddTaskModal = ({ open, onClose }) => {
   const [taskName, setTaskName] = useState('');
   const [lastTask, setLastTask] = useState(false);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
+  const [projects, setProjects] = useState([]);
   const [selectedProject, setSelectedProject] = useState('');
-  const [selectedEmployee ,setSelectedEmployee] = useState('');
+  const [selectedEmployee, setSelectedEmployee] = useState('');
+  const [employees, setEmployees] = useState([]);
 
   const handleClose = () => {
     onClose();
@@ -32,39 +35,54 @@ const AddTaskModal = ({ open, onClose }) => {
     onClose();
   };
 
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  const fetchProjects = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/api/getProjectNames');
+      setProjects(response.data);
+    } catch (error) {
+      console.error('Error fetching projects:', error);
+    }
+  };
+
   return (
     <Dialog open={open} onClose={handleClose} aria-labelledby="addnewTitle">
-      <DialogTitle id="addnewtask" style={{ textAlign: 'left', fontFamily: 'Nunito', color: '#4e73df',fontWeight:'700', fontSize:'30px'}}>
-            Add New Task</DialogTitle>
+      <DialogTitle id="addnewtask" style={{ textAlign: 'left', fontFamily: 'Nunito', color: '#4e73df', fontWeight: '700', fontSize: '30px' }}>
+        Add New Task</DialogTitle>
       <DialogContent>
-      <div>
-          <FormControl style={{ marginTop: '1rem' , marginRight:'3rem'}}>
+        <div>
+          <FormControl style={{ marginTop: '1rem', marginRight: '3rem' }}>
             <InputLabel >Select Project</InputLabel>
             <Select
               label="Select Project"
               id="addprojdrop"
               value={selectedProject}
               onChange={(e) => setSelectedProject(e.target.value)}
-              
             >
-              <MenuItem value="Unassigned/ No Work">Unassigned/ No Work</MenuItem>
+              {projects.map((project, index) => (
+                <MenuItem key={index} value={project}>
+                  {project}
+                </MenuItem>
+              ))}
             </Select>
           </FormControl>
-          <FormControl style={{ marginTop: '1rem'}}>
+          <FormControl style={{ marginTop: '1rem' }}>
             <InputLabel >Select Employee</InputLabel>
             <Select
-            id="selempdrop"
+              id="selempdrop"
               label="Select Employee"
               value={selectedEmployee}
               onChange={(e) => setSelectedEmployee(e.target.value)}
             >
-              <MenuItem value="">Select Employee</MenuItem>
-              <MenuItem value="55">Saurabh Patil</MenuItem>
+              
             </Select>
           </FormControl>
         </div>
-        <InputLabel  style={{ fontFamily: 'Nunito', color: '#4e73df', fontWeight: '700', fontSize: '18px' , marginTop:'1rem' }}>Task Details</InputLabel>
-        <InputLabel  style={{ fontFamily: 'Nunito', color: 'Black', fontWeight: '700', fontSize: '18px' }}>Task Name</InputLabel>
+        <InputLabel style={{ fontFamily: 'Nunito', color: '#4e73df', fontWeight: '700', fontSize: '18px', marginTop: '1rem' }}>Task Details</InputLabel>
+        <InputLabel style={{ fontFamily: 'Nunito', color: 'Black', fontWeight: '700', fontSize: '18px' }}>Task Name</InputLabel>
         <TextField
           autoFocus
           margin="dense"
@@ -87,7 +105,7 @@ const AddTaskModal = ({ open, onClose }) => {
         />
         <InputLabel style={{ fontFamily: 'Nunito', color: 'Black', fontWeight: '700', fontSize: '18px' }}>Time Required For Task</InputLabel>
         <TextField
-          style={{marginRight:'1rem'}}
+          style={{ marginRight: '1rem' }}
           margin="dense"
           id="addprojhr"
           type="number"
@@ -95,14 +113,14 @@ const AddTaskModal = ({ open, onClose }) => {
           onChange={(e) => setMinutes(e.target.value)}
           InputProps={{
             endAdornment: (
-              <InputAdornment position="end" style={{ outline: 'none'}}>Hr</InputAdornment>
+              <InputAdornment position="end" style={{ outline: 'none' }}>Hr</InputAdornment>
             ),
             style: { fontFamily: 'Nunito', color: 'black', fontWeight: '700', fontSize: '18px', border: 'none' },
             inputProps: { min: 0, max: 12, style: { padding: '25px' } }
           }}
         />
         <TextField
-          
+
           margin="dense"
           id="addprojmin"
           type="number"
