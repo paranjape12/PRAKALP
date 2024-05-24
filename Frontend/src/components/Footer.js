03cc87f2d364cff76f028a2101275352b34ffa72
Divya Kelaskar <divyakelaskar@gmail.com>
2024-05-24 17:16:18 +0530

    increased margin top

--- Frontend/src/components/Footer.js
+++ Frontend/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer className="sticky-footer bg-white p-1" style={{fontFamily:'Nunito',fontSize:'13px', textAlign:'center'}}>
+        <footer className="sticky-footer bg-white p-1" style={{fontFamily:'Nunito',fontSize:'13px', textAlign:'center', marginTop:'2rem'}}>
             <div className="container my-auto">
                 <div className="copyright text-center my-auto">
                     <span>Copyright © Protovec Technologies Pvt. Ltd. All rights reserved 2021</span>
