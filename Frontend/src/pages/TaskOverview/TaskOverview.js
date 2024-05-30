0452c8711d7e5598762135cc815d821514b763de
Divya Kelaskar <divyakelaskar@gmail.com>
2024-05-30 22:21:08 +0530

    Add Task Time Formatting and Status Indicator in TaskOverview page

--- Frontend/src/pages/TaskOverview/TaskOverview.js
+++ Frontend/src/pages/TaskOverview/TaskOverview.js
@@ -17,13 +17,31 @@ const daysOfWeek = [
   'Sat',
 ];
 
+const seconds2dayhrmin = (ss) => {
+  const s = ss % 60;
+  const h = Math.floor((ss % 28800) / 3600); // 8 hours = 28,800 seconds
+  const d = Math.floor((ss % 230400) / 28800); // 8 hours * 30 days = 230,400 seconds
+  const m = Math.floor((ss % 3600) / 60);
+
+  const formattedH = h < 10 ? '0' + h : h;
+  const formattedM = m < 10 ? '0' + m : m;
+  const formattedD = d < 10 ? '0' + d : d;
+
+  return ` ${formattedD} : ${formattedH} : ${formattedM} `;
+};
+
 function TaskOverview() {
   const [showComplete, setShowComplete] = useState(true);
+  const [showTimeComplete, setShowTimeComplete] = useState(true);
   const [projects, setProjects] = useState([]);
 
   const toggleShowComplete = (e) => {
     e.stopPropagation();
     setShowComplete(prevShowComplete => !prevShowComplete);
+  };
+  const toggleShowTimeComplete = (e) => {
+    e.stopPropagation();
+    setShowTimeComplete(prevShowTimeComplete => !prevShowTimeComplete);
   };
 
   const [dates, setDates] = useState([]);
@@ -101,6 +119,17 @@ function TaskOverview() {
     }
   };
 
+  function getTaskStatusColor(requiredTime, takenTime) {
+    if (requiredTime < takenTime) {
+      return 'bg-danger border border-danger';
+    } else if (takenTime === 0) {
+      return 'bg-warning border border-warning';
+    } else {
+      return 'bg-success border border-success';
+    }
+  }
+
+
   return (
     <div>
       {dates.length > 0 && (
@@ -114,7 +143,7 @@ function TaskOverview() {
         <thead className="text-white" id="theader">
           <tr className="text-center small">
             <th style={{ width: '20rem', verticalAlign: 'revert', color: 'white' }}>Projects</th>
-            <th style={{ width: '15rem', verticalAlign: 'revert', color: 'white', position: 'relative' }}>
+            <th style={{ width: '10rem', verticalAlign: 'revert', color: 'white', position: 'relative' }}>
               <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative' }}>
                 <span style={{ flexGrow: 1, textAlign: 'center' }}>Task Details</span>
                 <div className="taskEye" style={{ position: 'absolute', right: '1rem' }}>
@@ -148,7 +177,7 @@ function TaskOverview() {
           {projects.map((project, index) => (
             <tr key={index}>
               <td className="text-left" style={{ backgroundColor: getBackgroundColor(project.proj_status), color: 'black', padding: '0 0 0 0.5rem', fontSize: '13.44px' }}>
-                {project.assigntaskpresent && (<FontAwesomeIcon icon={faPlus} style={{ cursor: 'pointer' }} />)} [{project.projectSalesOrder}]
+                {project.assigntaskpresent && (<FontAwesomeIcon icon={faPlus} style={{ cursor: 'pointer' }} title='Expand Tasks' />)} [{project.projectSalesOrder}]
                 <a className="deleteproj p-1" style={{ float: 'right', cursor: 'pointer' }} title="Delete project" name={project.proid}>
                   <FontAwesomeIcon icon={faTrashAlt} className="text-danger" />
                 </a>
@@ -158,12 +187,47 @@ function TaskOverview() {
                 <br />
                 {project.projectName}
               </td>
+              {project.assigntaskpresent && (
+                <>
+                  <td style={{ width: '15rem', verticalAlign: 'unset', height: '100%', display: 'flex', alignItems: 'center' }} className="p-0">
+                    <div className="card" style={{ flex: '1', height: '100%' }}>
+                      <div className="card-header p-0">
+                        <h6 className={`m-0 text-center font-weight-bold text-white ${getTaskStatusColor(project.requiredTime, project.takenTime)}`} style={{ fontSize: '11px' }}>
+                          Total Task: {project.noofassigntasks}
+                          <a className="show p-0" style={{ float: 'right' }} title="Show/Hide Time" name="31">
+                            <div className="taskEye" style={{ position: 'absolute', right: '1rem' }}>
+                              <FontAwesomeIcon
+                                icon={showTimeComplete ? faEye : faEyeSlash}
+                                className="eyeicon"
+                                style={{ cursor: 'pointer', color: 'blue' }}
+                                onClick={toggleShowTimeComplete}
+                              />
+                            </div>
+                          </a>
+                        </h6>
+                      </div>
+                      <div className="card-body text-left p-0">
+                        <h6 title="Required" className="text-left m-0 Required" style={{ fontSize: '11px' }}>R : {seconds2dayhrmin(project.requiredTime)}</h6>
+                        <h6 title="Taken" className="text-left m-0 Taken" style={{ fontSize: '11px' }}>T : {seconds2dayhrmin(project.takenTime)}</h6>
+                      </div>
+                    </div>
+                    <div style={{ verticalAlign: 'middle', flex: '0 0 auto', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                      <td style={{ padding: '0 0.4rem', display: 'block', backgroundColor: 'gray', color: 'white', fontSize: '13.44px' }}>P
+                      </td>
+                      <td style={{ padding: '0 0.35rem', fontSize: '13.44px' }}>A
+                      </td>
+                    </div>
+                  </td>
+
+                </>
+              )}
+              {!project.assigntaskpresent && (<td className="text-center addtask" name={project.projectName} style={{ fontSize: '13.44px' }} colSpan="9">No Task Found today</td>)}
             </tr>
           ))}
         </tbody>
       </table>
       <Footer />
-    </div>
+    </div >
   );
 }
 
