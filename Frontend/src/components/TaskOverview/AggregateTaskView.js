62fabe001cb8b35878f675573b900aa47d2bfe9b
Divya Kelaskar <divyakelaskar@gmail.com>
2024-06-05 20:09:09 +0530

    minor frontend changes

--- Frontend/src/components/TaskOverview/AggregateTaskView.js
+++ Frontend/src/components/TaskOverview/AggregateTaskView.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
-const AggregateTaskView = ({ project, toggleShowTimeComplete, showTimeDetails, getTaskStatusColor, seconds2dayhrmin }) => {
+const AggregateTaskView = ({ project, toggleShowTimeComplete, seconds2dayhrmin }) => {
   const [localShowTimeDetails, setLocalShowTimeDetails] = useState(() => {
     const storedValue = localStorage.getItem('showTimeDetails');
     const details = storedValue ? JSON.parse(storedValue) : {};
@@ -26,12 +26,22 @@ const AggregateTaskView = ({ project, toggleShowTimeComplete, showTimeDetails, g
     toggleShowTimeComplete(project.projectId); // Keep the parent state in sync
   };
 
+  function getTaskStatusColor(requiredTime, takenTime) {
+    if (requiredTime < takenTime) {
+      return 'bg-danger border border-danger';
+    } else if (takenTime === 0) {
+      return 'bg-warning border border-warning';
+    } else {
+      return 'bg-success border border-success';
+    }
+  }
+
   return (
     <>
-      <td style={{ width: '15rem', verticalAlign: 'unset', height: '100%', display: 'flex', alignItems: 'center' }} className="p-0">
+      <td style={{ width: '15rem', verticalAlign: 'unset', paddingBottom: 'auto', display: 'flex', alignItems: 'center' }} className="p-0">
         <div className="card" style={{ flex: '1', height: '100%' }}>
           <div className="card-header p-0">
-            <h6 className={`m-0 text-center font-weight-bold text-white ${getTaskStatusColor(project.requiredTime, project.takenTime)}`} style={{ fontSize: '11px' }}>
+            <h6 className={`m-0 text-center font-weight-bold text-white ${getTaskStatusColor(project.requiredTime, project.takenTime)}`} style={{ fontSize: '11px', paddingTop:'0.1rem' }}>
               Total Task: {project.noofassigntasks}
               <a className="show p-0" style={{ float: 'right' }} title="Show/Hide Time" name="31">
                 <div className="taskEye" style={{ position: 'absolute', right: '1rem' }}>
@@ -45,7 +55,7 @@ const AggregateTaskView = ({ project, toggleShowTimeComplete, showTimeDetails, g
               </a>
             </h6>
           </div>
-          <div className="card-body text-left p-0">
+          <div className="card-body text-left" style={{ padding: '0.37rem' }}>
             {localShowTimeDetails && (
               <>
                 <h6 title="Required" className="text-left m-0 Required" style={{ fontSize: '11px' }}>R : {seconds2dayhrmin(project.requiredTime)}</h6>
@@ -54,9 +64,9 @@ const AggregateTaskView = ({ project, toggleShowTimeComplete, showTimeDetails, g
             )}
           </div>
         </div>
-        <div style={{ verticalAlign: 'middle', flex: '0 0 auto', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <td style={{ padding: '0 0.4rem', display: 'block', backgroundColor: 'gray', color: 'white', fontSize: '13.44px' }}>P</td>
-          <td style={{ padding: '0 0.35rem', fontSize: '13.44px' }}>A</td>
+        <div style={{ verticalAlign: 'middle', height: 'auto', display: 'flex', flexDirection: 'column'}}>
+          <td style={{ padding: '0.23rem 0.4rem', display: 'block', backgroundColor: 'gray', color: 'white', fontSize: '13.44px' }}>P</td>
+          <td style={{ padding: '0.23rem 0.35rem', fontSize: '13.44px' }}>A</td>
         </div>
       </td>
     </>
