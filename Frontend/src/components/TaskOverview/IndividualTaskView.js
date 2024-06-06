49ead55bdb695119f2dec9353c84651ec4a37c1d
Divya Kelaskar <divyakelaskar@gmail.com>
2024-06-06 19:45:53 +0530

    add handlers for task info dialog

--- Frontend/src/components/TaskOverview/IndividualTaskView.js
+++ Frontend/src/components/TaskOverview/IndividualTaskView.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import TaskInfoDialog from './TaskInfoDialog';
 import { faEye, faEyeSlash, faCopyright, faEdit, faCircleInfo, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 function getTaskStatusColor(status, aproved) {
@@ -15,6 +16,15 @@ function getTaskStatusColor(status, aproved) {
 
 const IndividualTaskView = ({ project, task, toggleShowTimeComplete, seconds2dayhrmin }) => {
   const [localShowTimeDetails, setLocalShowTimeDetails] = useState(false);
+  const [taskInfoDialogOpen, setTaskInfoDialogOpen] = useState(false);
+
+  const handleTaskInfoDialogOpen = () => {
+    setTaskInfoDialogOpen(true);
+  };
+
+  const handleTaskInfoDialogClose = () => {
+    setTaskInfoDialogOpen(false);
+  };
 
   const handleToggleShowTimeComplete = (e) => {
     e.stopPropagation();
@@ -24,18 +34,20 @@ const IndividualTaskView = ({ project, task, toggleShowTimeComplete, seconds2day
   return (
     <td style={{ width: '15rem', verticalAlign: 'top', height: '100%', display: 'flex', alignItems: 'center' }} className="p-0">
       <div className="card" style={{ flex: '1', height: '100%' }}>
-        <div className="card-header p-0" style={{ height: 'auto' }}>
+        <div className="card-header" style={{ height: 'auto', padding:'0.2rem'}}>
           <h6 className={`m-0 py-1 text-center font-weight-bold text-white ${getTaskStatusColor(task.taskStatus, task.taskAproved)}`} style={{ fontSize: '11px' }}>
             {task.taskName}
           </h6>
           <FontAwesomeIcon icon={faTrashCan} style={{ float: 'right', cursor: 'pointer', color: 'red', paddingTop: '0.2rem', paddingLeft: '0.5rem', paddingRight: '0.4rem' }} />
           <FontAwesomeIcon icon={faEdit} style={{ float: 'right', cursor: 'pointer', color: '#4e73df', paddingTop: '0.2rem', paddingLeft: '0.5rem' }} />
           <FontAwesomeIcon icon={faCopyright} color='#1cc88a' style={{ marginLeft: '0.4rem', verticalAlign: 'middle' }} />
-          <FontAwesomeIcon icon={faCircleInfo} style={{ float: 'right', cursor: 'pointer', color: '#4e73df', paddingTop: '0.2rem', paddingLeft: '0.5rem' }} />
+          <FontAwesomeIcon icon={faCircleInfo} style={{ float: 'right', cursor: 'pointer', color: '#4e73df', paddingTop: '0.2rem', paddingLeft: '0.5rem' }} onClick={handleTaskInfoDialogOpen}/>
           <FontAwesomeIcon title='Show/Hide Time' icon={localShowTimeDetails ? faEyeSlash : faEye} onClick={handleToggleShowTimeComplete}
             style={{ float: 'right', cursor: 'pointer', color: '#4e73df', paddingTop: '0.2rem' }}
           />
         </div>
+        
+      <TaskInfoDialog open={taskInfoDialogOpen} handleClose={handleTaskInfoDialogClose} />
         {localShowTimeDetails && (
           <div className="card-body text-left p-0" style={{ marginLeft: '0.4rem', fontSize: '11px' }}>
             R: {seconds2dayhrmin(task.taskGivenTime)}
