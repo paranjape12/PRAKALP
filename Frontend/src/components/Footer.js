b49fbaf043a0792f68b2dd539e31fa63feef9130
hrishi0351 <hrishikeshshelar351@gmail.com>
2024-07-02 18:05:13 +0530

    width add to footer

--- Frontend/src/components/Footer.js
+++ Frontend/src/components/Footer.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer className="sticky-footer bg-white p-1" style={{fontFamily:'Nunito',fontSize:'13px', textAlign:'center', marginTop:'2rem'}}>
+       
+        <footer className="sticky-footer bg-white p-1" style={{fontFamily:'Nunito',fontSize:'13px', textAlign:'center', marginTop:'1rem',width:'100%'}}>
             <div className="container my-auto">
                 <div className="copyright text-center my-auto">
                     <span>Copyright © Protovec Technologies Pvt. Ltd. All rights reserved 2021</span>
