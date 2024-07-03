84c69e4bf707e6c61dbc5cca08bb2690bd01b28b
hrishi0351 <hrishikeshshelar351@gmail.com>
2024-07-03 16:06:52 +0530

    add route EmployeeOverView and ProjectOverView

--- Frontend/src/routes/RouteManager.js
+++ Frontend/src/routes/RouteManager.js
@@ -4,12 +4,13 @@ import '../App.css';
 import HomePage from '../pages/HomePage';
 import AddEmployee from '../components/Navbar/Dropdown/Manage Employee/AddEmployee';
 import TaskOveriew from '../pages/TaskOverview/TaskOverview';
-import EmployeePage from '../pages/EmployeePage';
+//import EmployeePage from '../pages/EmployeePage';
 import Login from '../pages/Login/Login';
 import EditEmployee from '../components/Navbar/Dropdown/Manage Employee/EditEmployee';
 import Register from '../pages/Register/Register';
-import ProjectOverview from '../pages/ProjectOverview';
+import ProjectOverview from '../pages/ProjectOverview/ProjectOverview';
 import Profile from '../pages/Profile/Profile';
+import EmployeeOverview from '../pages/EmployeeOverview/EmployeeOverview';
 
 function RouteManager() {
     const [showAddProject, setShowAddProject] = useState(false);
@@ -23,7 +24,7 @@ function RouteManager() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/profile" element={<Profile/>} />
                 <Route path="/task" element={<TaskOveriew/>} />
-                <Route path="/employee" element={<EmployeePage isPopupVisible={showAddProject || showAddTask || showAssignTask} />} />
+                <Route path="/employeeOverview" element={<EmployeeOverview isPopupVisible={showAddProject || showAddTask || showAssignTask} />} />
                 <Route path="/project" element={<ProjectOverview />} />
 
 
