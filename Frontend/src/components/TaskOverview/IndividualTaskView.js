64e196e6fdf07d809c50aeeaad92518828671fc3
Divya Kelaskar <divyakelaskar@gmail.com>
2024-07-04 10:44:03 +0530

    individual task frontend done

--- Frontend/src/components/TaskOverview/IndividualTaskView.js
+++ Frontend/src/components/TaskOverview/IndividualTaskView.js
@@ -16,15 +16,12 @@ function getTaskStatusColor(status, approved) {
   }
 }
 
-
 const IndividualTaskView = ({ project, task, toggleShowTimeComplete, seconds2dayhrmin }) => {
   const [localShowTimeDetails, setLocalShowTimeDetails] = useState(false);
   const [taskInfoDialogOpen, setTaskInfoDialogOpen] = useState(false);
   const [selectedProject, setSelectedProject] = useState({});
   const [deleteTaskDialogOpen, setDeleteTaskDialogOpen] = useState(false);
   const [taskToDelete, setTaskToDelete] = useState(null);
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
   const [editTaskDialogOpen, setEditTaskDialogOpen] = useState(false);
   const [taskDetails, setTaskDetails] = useState(null);
 
@@ -54,7 +51,6 @@ const IndividualTaskView = ({ project, task, toggleShowTimeComplete, seconds2day
   };
 
   const handleOpenEditTaskDialog = () => {
-
     setSelectedProject({
       projectName: project.projectName,
       projectLastTask: project.projectLastTask,
@@ -64,7 +60,6 @@ const IndividualTaskView = ({ project, task, toggleShowTimeComplete, seconds2day
     });
     setEditTaskDialogOpen(true);
   };
-
 
   const handleCloseEditTaskDialog = () => {
     setSelectedProject(null);
@@ -87,60 +82,74 @@ const IndividualTaskView = ({ project, task, toggleShowTimeComplete, seconds2day
   };
 
   return (
-    <td style={{ width: '15rem', verticalAlign: 'top', height: '100%', display: 'flex', alignItems: 'center', border:'none' }} className="p-0">
-      <div className="card" style={{ flex: '1', height: '100%' }}>
-        <div className="card-header" style={{ height: 'auto', padding: '0.2rem' }}>
-          <h6 className={`m-0 py-1 text-center font-weight-bold text-white ${getTaskStatusColor(task.taskStatus, task.taskAproved)}`} style={{ fontSize: '11px' }}>
-            {task.taskName}
-          </h6>
-          <FontAwesomeIcon icon={faTrashCan} style={{ float: 'right', cursor: 'pointer', color: 'red', paddingTop: '0.2rem', paddingLeft: '0.5rem', paddingRight: '0.4rem' }} onClick={handleOpenDeleteTaskDialog} />
-          <FontAwesomeIcon icon={faPencilAlt} style={{ float: 'right', cursor: 'pointer', color: '#4e73df', paddingTop: '0.2rem', paddingLeft: '0.5rem' }} onClick={handleOpenEditTaskDialog} />
-          {task.taskStatus === "1" && (<FontAwesomeIcon icon={faCopyright} color='#1cc88a' style={{ marginLeft: '0.4rem', verticalAlign: 'middle' }} />)}
-          <FontAwesomeIcon icon={faCircleInfo} style={{ float: 'right', cursor: 'pointer', color: '#4e73df', paddingTop: '0.2rem', paddingLeft: '0.5rem' }} onClick={handleTaskInfoDialogOpen} />
-          {project.projectLastTask === 1 && (<FontAwesomeIcon icon={faL} style={{ color: '#36b9cc', paddingTop: '0.2rem', paddingLeft: '0.5rem' }} />)}
-          <FontAwesomeIcon title='Show/Hide Time' icon={localShowTimeDetails ? faEyeSlash : faEye} onClick={handleToggleShowTimeComplete}
-            style={{ float: 'right', cursor: 'pointer', color: '#4e73df', paddingTop: '0.2rem' }}
+    <div className="task-container" style={{ display: 'flex', flexDirection: 'column' }}>
+      <div className="p-0" style={{ width: '100%', verticalAlign: 'top', height: '100%', display: 'flex', border: 'none' }}>
+        <div className="card" style={{ flex: '1', height: '2rem', display: 'flex', flexDirection: 'column', width: '11rem', marginLeft: '0' }}>
+          <div style={{ height: '5rem', width: '13.2rem', border: 'none' }}>
+            <h6 className={`m-0 py-1 text-center font-weight-bold text-white ${getTaskStatusColor(task.taskStatus, task.taskAproved)}`} style={{ marginTop: '0.5rem', fontSize: '11px' }}>
+              {task.taskName}
+            </h6>
+            <FontAwesomeIcon icon={faTrashCan} style={{ float: 'right', cursor: 'pointer', color: 'red', paddingTop: '0.2rem', paddingLeft: '0.5rem', paddingRight: '0.4rem' }} onClick={handleOpenDeleteTaskDialog} />
+            <FontAwesomeIcon icon={faPencilAlt} style={{ float: 'right', cursor: 'pointer', color: '#4e73df', paddingTop: '0.2rem', paddingLeft: '0.5rem' }} onClick={handleOpenEditTaskDialog} />
+            {task.taskStatus === "1" && (<FontAwesomeIcon icon={faCopyright} color='#1cc88a' style={{ marginLeft: '0.4rem', verticalAlign: 'middle' }} />)}
+            <FontAwesomeIcon icon={faCircleInfo} style={{ float: 'right', cursor: 'pointer', color: '#4e73df', paddingTop: '0.2rem', paddingLeft: '0.5rem' }} onClick={handleTaskInfoDialogOpen} />
+            {project.projectLastTask === 1 && (<FontAwesomeIcon icon={faL} style={{ color: '#36b9cc', paddingTop: '0.2rem', paddingLeft: '0.5rem' }} />)}
+            <FontAwesomeIcon title='Show/Hide Time' icon={localShowTimeDetails ? faEyeSlash : faEye} onClick={handleToggleShowTimeComplete} style={{ float: 'right', cursor: 'pointer', color: '#4e73df', paddingTop: '0.2rem' }} />
+          </div>
+
+          <TaskInfoDialog
+            key={task.taskId}
+            open={taskInfoDialogOpen}
+            project={project}
+            task={task}
+            taskDetails={taskDetails}
+            handleClose={handleTaskInfoDialogClose}
           />
+
+          {selectedProject && (
+            <EditTaskPopup
+              open={editTaskDialogOpen}
+              handleClose={handleCloseEditTaskDialog}
+              projectDetails={selectedProject}
+            />
+          )}
+
+          {deleteTaskDialogOpen && (
+            <DeleteTaskPopup
+              task={taskToDelete}
+              open={deleteTaskDialogOpen}
+              handleClose={handleCloseDeleteTaskDialog}
+            />
+          )}
+
+          {localShowTimeDetails && (
+            <div className="card-body text-left" style={{ padding: '0', fontSize: '11px', height: 'auto', width: '13rem', marginLeft: '0.2rem', marginBottom: '0.2rem' }}>
+              R: {seconds2dayhrmin(task.taskRequiredTime)}
+              <br />
+              T: {seconds2dayhrmin(task.taskActualTime)}
+            </div>
+          )}
         </div>
 
-        <TaskInfoDialog
-          key={task.taskId}
-          open={taskInfoDialogOpen}
-          project={project}
-          task={task}
-          taskDetails={taskDetails}
-          handleClose={handleTaskInfoDialogClose}
-        />
 
-        {selectedProject && (
-          <EditTaskPopup
-            open={editTaskDialogOpen}
-            handleClose={handleCloseEditTaskDialog}
-            projectDetails={selectedProject}
-          />
-        )}
-
-        {deleteTaskDialogOpen && (
-          <DeleteTaskPopup
-            task={taskToDelete}
-            open={deleteTaskDialogOpen}
-            handleClose={handleCloseDeleteTaskDialog}
-          />
-        )}
-
-        {localShowTimeDetails && (
-          <div className="card-body text-left p-0" style={{ marginLeft: '0.4rem', fontSize: '11px' }}>
-            R: {seconds2dayhrmin(task.taskGivenTime)}
-            <br />
-            T: {seconds2dayhrmin(task.taskActualTime)}
-          </div>
-        )}
+        <table style={{ marginLeft: '13.2rem' }}>
+          <tbody>
+            <tr>
+              <td style={{ padding: '0.73rem 0.5rem', display: 'block', backgroundColor: 'gray', color: 'white', fontSize: '13.44px', borderStyle: 'none solid none none' }}>P</td>
+              {[...Array(7)].map((_, i) => (
+                <td key={i} style={{ padding: '0.6rem 4.19rem', backgroundColor: 'gray', color: 'white', borderStyle: 'none solid solid none', textAlign: 'center' }}>&nbsp;</td>
+              ))}
+            </tr>
+            <tr>
+              <td style={{ padding: '0.6rem 0.5rem', fontSize: '13.44px' }}>A</td>
+              {[...Array(7)].map((_, i) => (
+                <td key={i} style={{ padding: '0.6rem 4.19rem', backgroundColor: 'white', border: '1px solid gray', textAlign: 'center' }}>&nbsp;</td>
+              ))}
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <div style={{ verticalAlign: 'middle', flex: '0 0 auto', display: 'flex', flexDirection: 'column' }}>
-        <td style={{ padding: '0.6rem 0.5rem', display: 'block', backgroundColor: 'gray', color: 'white', fontSize: '13.44px' }}>P</td>
-        <td style={{ padding: '0.6rem 0.5rem', fontSize: '13.44px' }}>A</td>
-      </div>
-    </td>
+    </div>
   );
 }
 
