0fb9d5fee79467d1f590652eda75d6c40e1b9f9d
Divya Kelaskar <divyakelaskar@gmail.com>
2024-07-23 18:15:17 +0530

    tons of changes, caching API data whereever possible

--- Frontend/src/components/EmployeeOverview/IndividualTableCellsView.js
+++ Frontend/src/components/EmployeeOverview/IndividualTableCellsView.js
@@ -2,17 +2,21 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash, faTrashAlt, faPencilAlt, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+import AggregateTaskDetailsView from './AggregateTaskDetailsView';
+import IndividualTaskDetailsView from './IndividualTaskDetailsView';
+import EditProjectPopup from '../TaskOverview/EditProjectPopup';
+import DeleteProjectPopup from '../TaskOverview/DeleteProjectPopup';
 
 const getBackgroundColor = (proj_status) => {
     switch (proj_status) {
         case 1:
             return '#ADD8E6';
         case 2:
-            return '#ffff00ad';
+            return '#ffff00';
         case 3:
-            return '#ff8d00b8';
+            return '#ff8d00';
         case 4:
-            return '#04ff00b8';
+            return '#04ff00';
         default:
             return '#FFFFFF';
     }
@@ -22,6 +26,7 @@ function IndividualTableCellsView({ employee, isComplete, dates }) {
     const [localShowTimeDetails, setLocalShowTimeDetails] = useState(true);
     const [totalTasks, setTotalTasks] = useState(0);
     const [projects, setProjects] = useState([]);
+    const [cachedProjects, setCachedProjects] = useState({});
     const [selectedProjectId, setSelectedProjectId] = useState(null);
     const [selectedProject, setSelectedProject] = useState(null);
     const [deleteProjectDialogOpen, setDeleteProjectDialogOpen] = useState(false);
@@ -35,27 +40,66 @@ function IndividualTableCellsView({ employee, isComplete, dates }) {
         actual: {}
     });
     const empId = employee.id;
+    const empType = employee.Type;
 
     useEffect(() => {
         const fetchProjects = async () => {
-            try {
-                const response = await axios.get('http://localhost:3001/api/EmpOverviewPlusMinus', {
-                    params: { empid: empId, U_type: 'User' } // Adjust U_type as necessary
-                });
-                setProjects(response.data);
-            } catch (error) {
-                console.error('Error fetching project data:', error);
+            if (cachedProjects[empId]) {
+                setProjects(cachedProjects[empId]);
+            } else {
+                try {
+                    const response = await axios.get('http://localhost:3001/api/EmpOverviewPlusMinus', {
+                        params: { empid: empId, U_type: empType }
+                    });
+                    setProjects(response.data);
+                    setCachedProjects(prevState => ({ ...prevState, [empId]: response.data }));
+                } catch (error) {
+                    console.error('Error fetching project data:', error);
+                }
             }
         };
 
         fetchProjects();
-    }, [empId]);
+    }, [empId, cachedProjects]);
 
     const handleExpandTasks = (projectId) => {
         setExpandedProjects((prevState) => ({
             ...prevState,
-            [projectId]: !prevState[projectId],
+            [projectId]: prevState[projectId] === 'individual' ? 'aggregate' : 'individual',
         }));
+    };
+
+    const handleSaveEditProject = async (updatedProject) => {
+        try {
+            const response = await axios.post(
+                "http://localhost:3001/api/updateProject",
+                {
+                    ProjectName: updatedProject.projectName,
+                    Projectid: updatedProject.projectId,
+                    projstatus: updatedProject.projectStatus,
+                    editprojmodalisalesval: updatedProject.salesOrder,
+                }
+            );
+
+            if (response.data === "Success") {
+                // Update the projects state after saving
+                setProjects((prevProjects) =>
+                    prevProjects.map((proj) =>
+                        proj.projectSalesOrder === updatedProject.salesOrder
+                            ? {
+                                ...proj,
+                                projectName: updatedProject.projectName,
+                                proj_status: updatedProject.projectStatus,
+                            }
+                            : proj
+                    )
+                );
+            } else {
+                console.error("Failed to update project:", response.data);
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
     };
 
     const handleOpenDeleteProjectDialog = (projectId, projectName) => {
@@ -107,77 +151,60 @@ function IndividualTableCellsView({ employee, isComplete, dates }) {
 
     return (
         <>
-            {projects.map((project) => (
-                <React.Fragment key={project.projectId}>
-                    <tr>
-                    <td style={{ fontSize: '13.5px', padding: '0',minWidth:'15rem' }}>
-                        <div className="text-left" style={{ backgroundColor: getBackgroundColor(project.proj_status), color: 'black', padding: '0.5rem', fontSize: '13.44px' }}>
-                            {project.assigntaskpresent && (
-                                <FontAwesomeIcon
-                                    icon={expandedProjects[project.projectId] ? faMinus : faPlus}
-                                    style={{ cursor: 'pointer' }}
-                                    title='Expand/Collapse Tasks'
-                                    onClick={() => handleExpandTasks(project.projectId)}
-                                />
-                            )}
-                            [{project.projectSalesOrder}]
-                            <a className="deleteproj p-1" style={{ float: 'right', cursor: 'pointer' }} title="Delete project" name={project.proid} onClick={() => handleOpenDeleteProjectDialog(project.projectId, project.projectName)}>
-                                <FontAwesomeIcon icon={faTrashAlt} className="text-danger" />
-                            </a>
-                            <a className="editproj p-1" style={{ float: 'right', cursor: 'pointer' }} title="Edit project" id={project.projectId} name={project.projectName} value={project.proj_status} onClick={() => handleOpenEditProjectDialog(project)}>
-                                <FontAwesomeIcon icon={faPencilAlt} className="text-primary" />
-                            </a>
-                            <br />
-                            {project.projectName}
-                        </div>
-                    </td>
-                    <td style={{minWidth:'14rem', padding:'0'}}>
-                        <div className="card">
-                            <div className="text-center" style={{paddingRight: '4rem', paddingLeft: '0.3rem', backgroundColor:'lavender' }}>
-                                <div style={{ fontSize: '14px' }} className="m-0 font-weight-bold text-left text-dark">
-                                    Total Task Assign: 555
-                                    <a className="show p-0" style={{ float: 'right' }} title="Show/Hide Time">
-                                        <div className="taskEye" style={{ position: 'absolute', right: '0.5rem' }}>
-                                            <FontAwesomeIcon
-                                                icon={localShowTimeDetails ? faEye : faEyeSlash}
-                                                className="eyeicon"
-                                                style={{ cursor: 'pointer', color: '#1e7ee4' }}
-                                                onClick={handleToggleShowTimeComplete}
-                                            />
-                                        </div>
+            {projects.length > 0 ? (
+                projects.map((project) => (
+                    <React.Fragment key={project.projectId}>
+                        <tr>
+                            <td style={{ fontSize: '13.5px', padding: '0', backgroundColor: getBackgroundColor(project.proj_status), minWidth: '15rem' }}>
+                                <div className="text-left" style={{ backgroundColor: getBackgroundColor(project.proj_status), color: 'black', padding: '0.5rem', fontSize: '13.44px' }}>
+                                    {project.assigntaskpresent && (
+                                        <FontAwesomeIcon
+                                            icon={expandedProjects[project.projectId] === 'individual' ? faMinus : faPlus}
+                                            style={{ cursor: 'pointer', marginRight:'0.2rem' }}
+                                            title='Expand/Collapse Tasks'
+                                            onClick={() => handleExpandTasks(project.projectId)}
+                                        />
+                                    )}
+                                    [{project.projectSalesOrder}]
+                                    <a className="deleteproj p-1" style={{ float: 'right', cursor: 'pointer' }} title="Delete project" name={project.proid} onClick={() => handleOpenDeleteProjectDialog(project.projectId, project.projectName)}>
+                                        <FontAwesomeIcon icon={faTrashAlt} className="text-danger" />
                                     </a>
+                                    <a className="editproj p-1" style={{ float: 'right', cursor: 'pointer' }} title="Edit project" id={project.projectId} name={project.projectName} value={project.proj_status} onClick={() => handleOpenEditProjectDialog(project)}>
+                                        <FontAwesomeIcon icon={faPencilAlt} className="text-primary" />
+                                    </a>
+                                    <br />
+                                    {project.projectName}
                                 </div>
-                            </div>
-                            <div className="card-body text-left p-1">
-                                {localShowTimeDetails && (
-                                    <>
-                                        <div title="Required">R: TBD</div>
-                                        <div title="Taken">T: TBD</div>
-                                    </>
-                                )}
-                            </div>
-                        </div>
-                    </td>
-                    <td style={{padding:'0'}}>
-                        <div style={{ verticalAlign: 'middle', height: 'auto', display: 'flex', flexDirection: 'column' }}>
-                            <div title='Planned Timings' style={{ padding: '0.4rem 0.5rem', display: 'block', backgroundColor: 'gray', color: 'white', fontSize: '13.44px', borderStyle: 'none none none solid' }}>P</div>
-                            <div title='Actual Timings' style={{ padding: '0.4rem 0.5rem', fontSize: '13.44px', borderStyle: 'solid none none solid' }}>A</div>
-                        </div>
-                    </td>
-                    
-                    {dates.map((date, i) => (
-                        <td key={i} style={{ padding: '0', fontSize: '15px', width: '7rem' }}>
-                            <div style={{ paddingTop: '0.2rem', paddingRight:'7.68rem', display: 'block', backgroundColor: 'gray', color: 'white', border: 'none', textAlign: 'center', height: '2rem', verticalAlign: 'middle' }}>
-                                {seconds2dayhrmin(projectTimeDetails.planned[date.ymdDate] || 0)}
-                            </div>
-                            <div style={{ paddingTop: '0.2rem', paddingRight:'7.68rem', display: 'block', borderStyle: 'solid none none none', textAlign: 'center', height: '2rem', verticalAlign: 'middle' }}>
-                                {seconds2dayhrmin(projectTimeDetails.actual[date.ymdDate] || 0)}
-                            </div>
-                        </td>
-                    ))}
-                    </tr>
-                </React.Fragment>
-            ))}
+                            </td>
+                            {expandedProjects[project.projectId] === 'individual' ? (
+                                <IndividualTaskDetailsView dates={dates} localShowTimeDetails={localShowTimeDetails} handleToggleShowTimeComplete={handleToggleShowTimeComplete}
+                                    seconds2dayhrmin={seconds2dayhrmin} projectTimeDetails={projectTimeDetails} project={project} employee={employee} />
+                            ) : (
+                                <AggregateTaskDetailsView dates={dates} localShowTimeDetails={localShowTimeDetails} handleToggleShowTimeComplete={handleToggleShowTimeComplete}
+                                    seconds2dayhrmin={seconds2dayhrmin} projectTimeDetails={projectTimeDetails} project={project} employee={employee} />
+                            )}
+                        </tr>
+                    </React.Fragment>
+                ))
+            ) : (
+                <td style={{ textAlign: 'center' }} colSpan={9}>No Projects Assigned.</td>
+            )}
+            {selectedProject && (
+                <EditProjectPopup
+                    open={editProjectDialogOpen}
+                    handleClose={handleCloseEditProjectDialog}
+                    projectDetails={selectedProject}
+                    onSave={handleSaveEditProject}
+                />
+            )}
+            {selectedProjectId && (
+                <DeleteProjectPopup
+                    open={deleteProjectDialogOpen}
+                    handleClose={handleCloseDeleteProjectDialog}
+                    selectedProjectId={selectedProjectId}
+                    projectName={projectName}
+                />
+            )}
         </>
     );
 }
