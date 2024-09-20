15f23d5c6d8d25d6f96251f5550e0790bb93ebd3
Divya Kelaskar <divyakelaskar@gmail.com>
2024-09-20 17:44:27 +0530

    added error handling

--- Backend/middleware/decryptToken.js
+++ Backend/middleware/decryptToken.js
@@ -1,8 +1,12 @@
 function decryptToken(token) {
+  try {
     const decodedToken = Buffer.from(token, 'base64').toString('utf-8');
     const userData = JSON.parse(decodedToken)[0];
     return userData;
+  } catch (error) {
+    console.error("Invalid token format:", error);
+    return null;
   }
-  
-  module.exports = decryptToken;
-  
+}
+
+module.exports = decryptToken;
