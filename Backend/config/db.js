dcb20422df837f46881be5c87511e6e603c9cce1
Divya Kelaskar <divyakelaskar@gmail.com>
2024-09-30 16:35:56 +0530

    implement env at backend

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,25 +1,12 @@
+require('dotenv').config(); 
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'indiscpx_taskdb_2',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
-
-// const db = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'myuser',
-//   password: 'Mypassword@1',
-//   database: 'indiscpx_taskdb_2',
-// });
-
-// const db = mysql.createConnection({
-//   host: '103.195.185.168',
-//   user: 'indiscpx_TASKDB_2',
-//   password: 'Protovec123',
-//   database: 'indiscpx_TASKDB_2',
-// });
 
 db.connect((err) => {
   if (err) {
@@ -30,3 +17,4 @@ db.connect((err) => {
 });
 
 module.exports = db;
+
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
+require('dotenv').config(); 
 const app = require('./app');
 const port = 3001;
-const ipAddr = '127.0.0.1';
+const ipAddr = process.env.IP_ADDR;
 
 app.listen(port, ipAddr, () => {
   console.log(`Server is running on http://${ipAddr}:${port}`);
